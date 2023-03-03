762230983ec1b239d852ba97d2f359bfd0c047b2
Nabil <nabil.noble01@gmail.com>
2023-03-03 12:57:30 +0000

    edit task

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -2,10 +2,10 @@ import redis from 'redis';
 const client = redis.createClient();
 
 client.on("error", (error) => {
-    if (error) console.log(`Redis client not connected to the server: ${error}`)
-  }).on('ready', () => {
-      console.log('Redis client connected to the server');
-  });
+  if (error) console.log(`Redis client not connected to the server: ${error}`)
+}).on('ready', () => {
+    console.log('Redis client connected to the server');
+});
 
 client.subscribe("holberton school channel");
 
